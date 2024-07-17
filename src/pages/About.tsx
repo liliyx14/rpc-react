6bb992d16bf28da028424335777cf32fcc6e8cc3
黎炎祥 <1460050616@qq.com>
2024-07-17 16:54:13 +0800

    前端项目搭建

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+
+const About: React.FC = () => {
+    return (
+        <div className="about">
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </div>)
+}
+export default About;
